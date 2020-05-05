9f42216bccd77ac76e6b459d35590e0d3ea49ba5
vntpede <magliomendes@gmail.com>
2020-05-05 15:23:50 -0300

    Updates

---
+++ precache-manifest.9dfb71f2406151079a1cec9e35e4884d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6772a59710fde693a770477e33ae821",
+    "url": "/react-with-redux/index.html"
+  },
+  {
+    "revision": "4f0954abbf92024d459c",
+    "url": "/react-with-redux/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4de77ea9f657a1dbc334",
+    "url": "/react-with-redux/static/css/main.0b1f01e5.chunk.css"
+  },
+  {
+    "revision": "4f0954abbf92024d459c",
+    "url": "/react-with-redux/static/js/2.e8d37ea5.chunk.js"
+  },
+  {
+    "revision": "4de77ea9f657a1dbc334",
+    "url": "/react-with-redux/static/js/main.454a2231.chunk.js"
+  },
+  {
+    "revision": "f416a6ac651601a3a2ca",
+    "url": "/react-with-redux/static/js/runtime~main.b1d4797e.js"
+  },
+  {
+    "revision": "ef11bd6cf6c55af0a5566c235f280d4f",
+    "url": "/react-with-redux/static/media/loader.ef11bd6c.svg"
+  }
+]);
